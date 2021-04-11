98f266c32a0b281ce205d3cda518bb789eaa666f
ankitautkarr <ankitautkarra@gmail.com>
2021-04-11 17:14:56 +0530

    input & output

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 console.log(txtInput);
+var outputDiv = document.querySelector('#output');
+
+
+
 
 function clickHandler() {
-  console.log('clicked!');
-  console.log('input', txtInput.value);
+  outputDiv.innerText="translated: " + txtInput.value
 }
 btnTranslate.addEventListener('click', clickHandler);
