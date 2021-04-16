44d65674198e051c9f57c3be9a456b721fa46de4
ankitautkarr <ankitautkarra@gmail.com>
2021-04-16 12:54:34 +0530

    update

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var txtInput = document.querySelector('#txt-input');
 console.log(txtInput);
 var outputDiv = document.querySelector('#output');
 
-
-
-
 function clickHandler() {
-  outputDiv.innerText="translated: " + txtInput.value
+  outputDiv.innerText = txtInput.value;
 }
 btnTranslate.addEventListener('click', clickHandler);
