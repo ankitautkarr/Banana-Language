560f197ff7565cfa37675cf1ebee50a3d5d66a31
ankitautkarr <ankitautkarra@gmail.com>
2021-04-16 16:27:58 +0530

    input output

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var btnTranslate = document.querySelector('#btn-translate');
-var txtInput = document.querySelector('#txt-input');
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 console.log(txtInput);
-var outputDiv = document.querySelector('#output');
+var outputDiv = document.querySelector("#output");
+
 
 function clickHandler() {
   outputDiv.innerText = txtInput.value;
 }
-btnTranslate.addEventListener('click', clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
