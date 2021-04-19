152a4bde253ede9bd1de5344d265fa2023af040b
ankitautkarr <ankitautkarra@gmail.com>
2021-04-19 17:37:43 +0530

    spelling check

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 // console.log(txtInput);
 var outputDiv = document.querySelector('#output');
-var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text
