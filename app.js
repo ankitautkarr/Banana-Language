57b1a5034887685f7afff6c8cca64c54f99d4cd1
ankitautkarr <ankitautkarra@gmail.com>
2021-04-19 17:30:51 +0530

    translation done

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
-var outputDiv = document.querySelector("#output");
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+// console.log(txtInput);
+var outputDiv = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
 
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+  console.log("erroe occured" , error );
+  alert("Something wrong with server. Please try after sometime.");
+}
 
 function clickHandler() {
-  outputDiv.innerText = txtInput.value;
-}
+  var inputText = txtInput.value;
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText= json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+      .catch(errorHandler)
+  }
 btnTranslate.addEventListener("click", clickHandler);
