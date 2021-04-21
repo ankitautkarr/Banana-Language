c0244659cc45a3394c3dd4f6179b871fb7f65366
ankitautkarr <ankitautkarra@gmail.com>
2021-04-21 17:44:02 +0530

    finale check

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 // console.log(txtInput);
 var outputDiv = document.querySelector('#output');
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = 'https://api.funtranslations.com/translate/minion.json';
 
 function getTranslationURL(text) {
-  return serverURL + "?" + "text=" + text
+  return serverURL + '?' + 'text=' + text;
 }
 
 function errorHandler(error) {
-  console.log("erroe occured" , error );
-  alert("Something wrong with server. Please try after sometime.");
+  // console.log("erroe occured" , error );
+  alert('Something wrong with server. Please try after sometime.');
 }
 
 function clickHandler() {
   var inputText = txtInput.value;
   fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-      var translatedText= json.contents.translated;
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
       outputDiv.innerText = translatedText;
     })
-      .catch(errorHandler)
-  }
-btnTranslate.addEventListener("click", clickHandler);
+    .catch(errorHandler);
+}
+//btnTranslate.addEventListener("click", clickHandler);
